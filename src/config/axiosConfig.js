71e87f81f96005aab9510d1af3ced18b3b725c08
thevasudev <vasudev082002@gmail.com>
2025-09-03 19:26:37 +0530

    api chanegs

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL:"http://localhost:4000/",
+    baseURL:"https://real-estate-backend-ebon.vercel.app/",
   timeout: 30000, // Optional: Timeout after 10 seconds
   headers: {
     "Content-Type": "application/json",
